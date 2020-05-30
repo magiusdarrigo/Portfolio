eaeba8556dbd7b753618b399671ae5b50b965afa
magiusdarrigo <magiusdarrigo@gmail.com>
2020-05-30 16:07:08 -0500

    jobs app up running

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'e4cd83e1e2!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
