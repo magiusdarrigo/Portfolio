ef3780e4d5824bfd0d4261a653192fbe2fc8e7ed
magiusdarrigo <magiusdarrigo@gmail.com>
2020-05-31 01:35:05 -0500

    done for today

--- blog/urls.py
+++ blog/urls.py
@@ -17,5 +17,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.allblogs, name='home')
+    path('', views.allblogs, name='home'),
+    path('<int:blog_id>/', views.detail, name='detail'),
 ]
